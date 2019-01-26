78be4e0ee0c840f8f02fa6652678fbb2b8f1b3e2
pangluo <59036386@qq.com>
2019-01-26 22:35:47 +0800

    a sample

---
+++ src/main/java/com/domo/BallGame/BallGame.java
@@ -0,0 +1,16 @@
+package com.domo.BallGame;
+
+import javax.swing.JFrame;
+
+public class BallGame extends JFrame{
+	void launchFrame() {
+		setSize(300,300);
+		setLocation(400,400);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		BallGame game =new BallGame();
+		game.launchFrame();
+	}
+}
--- src/main/java/domo/ma.java
+++
@@ -1,7 +0,0 @@
-package domo;
-
-public class ma {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
-}
