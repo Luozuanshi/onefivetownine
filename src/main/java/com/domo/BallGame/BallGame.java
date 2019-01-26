05e2ad7ebc102489d5b0faf71f09c213ccfd210d
pangluo <59036386@qq.com>
2019-01-26 22:54:17 +0800

    version 2

--- src/main/java/com/domo/BallGame/BallGame.java
+++ src/main/java/com/domo/BallGame/BallGame.java
@@ -1,8 +1,13 @@
 package com.domo.BallGame;
 
+import java.awt.*;
 import javax.swing.JFrame;
 
 public class BallGame extends JFrame{
+	 Image ball = Toolkit.getDefaultToolkit().getImage("images/ball.png");
+	 Image desk = Toolkit.getDefaultToolkit().getImage("images/desk.jpg");
+	 double  x=100;    //小球的横坐标
+	 double  y=100; //小球的纵坐标
 	void launchFrame() {
 		setSize(300,300);
 		setLocation(400,400);
@@ -12,6 +17,5 @@ public class BallGame extends JFrame{
 	public static void main(String[] args) {
 		BallGame game =new BallGame();
 		game.launchFrame();
-		
 	}
 }
