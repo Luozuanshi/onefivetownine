508f3e1dfdde71b67ce0dbce09fd7f661258d51b
pangluo <59036386@qq.com>
2019-01-27 16:01:05 +0800

    define variable ruler

---
+++ src/main/java/com/domo/DefinevariableIdentif/VariableIdentif.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.domo.DefinevariableIdentif;
+
+/**
+   * 项目名称：domo
+   *类名称：VariableIdentif
+ * @author Administrator
+ * @version 1.0
+   * 创建时间2019年1月27日下午3:59:49
+   * 类描述
+ */
+public class VariableIdentif {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
