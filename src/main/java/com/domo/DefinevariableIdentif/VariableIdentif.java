f4fbc547af6247b264eca7d4f5ce1b979bf8b572
pangluo <59036386@qq.com>
2019-01-27 16:10:22 +0800

    define variable ruler 1

--- src/main/java/com/domo/DefinevariableIdentif/VariableIdentif.java
+++ src/main/java/com/domo/DefinevariableIdentif/VariableIdentif.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package com.domo.DefinevariableIdentif;
 
 /**
@@ -9,16 +7,28 @@ package com.domo.DefinevariableIdentif;
  * @author Administrator
  * @version 1.0
    * 创建时间2019年1月27日下午3:59:49
-   * 类描述
+   * 类描述:变量的命令规则,变量的定义,变量的初始化
  */
 public class VariableIdentif {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//可以使用中文
+		String 你好="nihao";
+		//可以使用下划线
+		double _pi =3.1415926;
+		//可以使用$
+		int $age =18;
+		
+		//不可以使用数字开头
+//		int 123fixed = 13;Syntax error on token "123f", delete this token
+		
+		//同时定义多个变量
+		int max,min,hello;
+//		System.out.println(max);The local variable max may not have been initialized
+//		System.out.println(domo);domo cannot be resolved to a variable
 	}
-
+	
+	
 }
+
+
