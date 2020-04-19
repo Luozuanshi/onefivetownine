236af75fb559d50c94f0fff7f1e81b6ec4c49188
lzs <59036386@qq.com>
2020-04-19 16:20:39 +0800

    JPA 增删改查

--- JPA/jpa-day01/src/main/java/com/domoyun/test/JpaTest.java
+++ JPA/jpa-day01/src/main/java/com/domoyun/test/JpaTest.java
@@ -1,6 +1,7 @@
 package com.domoyun.test;
 
 import com.domoyun.domain.Customer;
+import com.domoyun.utils.JpaUtils;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
@@ -24,11 +25,17 @@ public class JpaTest {
     @Test
     public void save(){
 //        1.加载配置文件创建工厂（实体管理器工厂）对象
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("myJpa");
-//        2.通过实体管理器工厂获取实体管理器
-        EntityManager em = emfactory.createEntityManager();
+//        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("myJpa");
+////        2.通过实体管理器工厂获取实体管理器
+//        EntityManager em = emfactory.createEntityManager();
+        EntityManager em = JpaUtils.getEntityManager();
+        EntityManager em1 = JpaUtils.getEntityManager();
+        System.out.println(em);
+        System.out.println(em1);
 //        3.获取事务对象，开启事务
         EntityTransaction transaction = em.getTransaction();
+
+
         transaction.begin();
 //        4.完成增删改查操作
         Customer customer = new Customer();
@@ -40,8 +47,117 @@ public class JpaTest {
         transaction.commit();
 //        6.释放资源
         em.close();
-        emfactory.close();
+//        emfactory.close();
+    }
+    /**
+     * 根据id查询客户
+     *  使用find方法查询：
+     *      1.查询的对象就是当前客户对象本身
+     *      2.在调用find方法的时候，就会发送sql语句查询数据库
+     *
+     *  立即加载
+     *
+     *
+     */
+    @Test
+    public void findId(){
+//        1.通过工具类获取entityManager
+        EntityManager em = JpaUtils.getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        //2.开启事务
+        transaction.begin();
+        //3.增删改查 -- 根据id查询客户
+        /**
+         * find : 根据id查询数据
+         *      class：查询数据的结果需要包装的实体类类型的字节码
+         *      id：查询的主键的取值
+         */
+        Customer customer = em.find(Customer.class, 3L);
+
+        System.out.println(customer);
+        //4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
+    }
+    /**
+     * 根据id查询客户
+     *      getReference方法
+     *          1.获取的对象是一个动态代理对象
+     *          2.调用getReference方法不会立即发送sql语句查询数据库
+     *              * 当调用查询结果对象的时候，才会发送查询的sql语句：什么时候用，什么时候发送sql语句查询数据库
+     *
+     * 延迟加载（懒加载）
+     *      * 得到的是一个动态代理对象
+     *      * 什么时候用，什么使用才会查询
+     */
+    @Test
+    public void getReference(){
+//        1.通过工具类获取entityManager
+        EntityManager em = JpaUtils.getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        //2.开启事务
+        transaction.begin();
+        //3.增删改查 -- 根据id查询客户
+        /**
+         * find : 根据id查询数据
+         *      class：查询数据的结果需要包装的实体类类型的字节码
+         *      id：查询的主键的取值
+         */
+        Customer customer = em.getReference(Customer.class, 3L);
+
+        System.out.println(customer);
+        //4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
     }
 
+    /**
+     * 删除客户的案例
+     *
+     */
+    @Test
+    public  void testRemove() {
+        //1.通过工具类获取entityManager
+        EntityManager entityManager = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        //3.增删改查 -- 删除客户
+
+        //根据id查询客户
+        Customer customer = entityManager.find(Customer.class, 1l);
+        //删除客户
+        entityManager.remove(customer);
+
+        //4.提交事务
+        tx.commit();
+        //5.释放资源
+        entityManager.close();
+    }
+    /**
+     * 更新客户的操作
+     *      merge(Object)
+     */
+    @Test
+    public  void testUpdate() {
+        //1.通过工具类获取entityManager
+        EntityManager entityManager = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        //3.增删改查 -- 更新操作
+
+        //i 查询客户
+        Customer customer = entityManager.find(Customer.class,1l);
+        //ii 更新客户
+        customer.setCustName("pangluo");
+        entityManager.merge(customer);
+        //4.提交事务
+        tx.commit();
+        //5.释放资源
+        entityManager.close();
+    }
 
 }
---
+++ JPA/jpa-day01/src/main/java/com/domoyun/test/jpqlTest.java
@@ -0,0 +1,160 @@
+package com.domoyun.test;
+
+import com.domoyun.domain.Customer;
+import com.domoyun.utils.JpaUtils;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import java.util.List;
+
+
+public class jpqlTest {
+    /**
+     * 查询全部
+     *      jqpl：from ccom.domoyun.domain.Customer
+     *      sql：SELECT * FROM cst_customer
+     */
+    @Test
+    public void findAll(){
+        //1.获取entityManager对象
+        EntityManager em = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction transaction = em.getTransaction();
+        //3.查询全部
+        transaction.begin();
+//        String jpql = "from com.domoyun.domain.Customer";
+        String jpql = "from Customer";
+        Query query = em.createQuery(jpql);
+        //发送查询，并封装结果集
+        List<Customer> list = query.getResultList();
+        for (Customer obj :list) {
+            System.out.println(obj.getCustName());
+            System.out.println(obj);
+        }
+        //4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
+    }
+    /**
+     * 排序查询： 倒序查询全部客户（根据id倒序）
+     *      sql：SELECT * FROM cst_customer ORDER BY cust_id DESC
+     *      jpql：from Customer order by custId desc
+     *
+     * 进行jpql查询
+     *      1.创建query查询对象
+     *      2.对参数进行赋值
+     *      3.查询，并得到返回结果
+     */
+    @Test
+    public void oderBy(){
+        //1.获取entityManager对象
+        EntityManager em = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction transaction = em.getTransaction();
+        //3.查询全部
+        transaction.begin();
+//        String jpql = "from com.domoyun.domain.Customer";
+        String jpql = "from Customer order by custId desc ";
+        Query query = em.createQuery(jpql);
+        //发送查询，并封装结果集
+        List<Customer> list = query.getResultList();
+        for (Customer obj :list) {
+            System.out.println(obj.getCustName());
+            System.out.println(obj);
+        }
+        //4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
+    }
+    /**
+     * 使用jpql查询，统计客户的总数
+     *      sql：SELECT COUNT(cust_id) FROM cst_customer
+     *      jpql：select count(custId) from Customer
+     */
+    @Test
+    public void count(){
+        //1.获取entityManager对象
+        EntityManager em = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction transaction = em.getTransaction();
+        //3.查询全部
+        transaction.begin();
+        //i  根据jpql获取Query对象
+        String jpql = "select count(custId) from Customer";
+        Query query = em.createQuery(jpql);
+        //ii 对参数赋值
+        //iii 获取结果集
+        Object obj = query.getSingleResult();
+        System.out.println(obj);
+        // 4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
+    }
+    /**
+     * 分页查询
+     *      sql：select * from cst_customer limit 0,3
+     *      jqpl : from Customer
+     */
+    @Test
+    public void pageinfo(){
+        //1.获取entityManager对象
+        EntityManager em = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction transaction = em.getTransaction();
+        //3.查询全部
+        transaction.begin();
+        //i  根据jpql获取Query对象
+        String jpql = "from Customer ";
+        Query query = em.createQuery(jpql);
+        //ii 对参数赋值
+        //起始索引
+        query.setFirstResult(1);
+        //每页查询的条数
+        query.setMaxResults(3);
+        //iii 获取结果集
+        List<Customer> result = query.getResultList();
+        for(Customer customer : result){
+            System.out.println(customer);
+        }
+        // 4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
+    }
+
+    /**
+     * 条件查询
+     *     案例：查询客户名称以‘传智播客’开头的客户
+     *          sql：SELECT * FROM cst_customer WHERE cust_name LIKE  ?
+     *          jpql : from Customer where custName like ?
+     */
+    @Test
+    public void conditions(){
+        //1.获取entityManager对象
+        EntityManager em = JpaUtils.getEntityManager();
+        //2.开启事务
+        EntityTransaction transaction = em.getTransaction();
+        //3.查询全部
+        transaction.begin();
+        //i  根据jpql获取Query对象
+        String jpql = "from Customer where custName like ?";
+        Query query = em.createQuery(jpql);
+        //ii 对参数赋值 -- 对占位符赋值
+        query.setParameter(1,"d%");
+        //iii 获取结果集
+        List<Customer> result = query.getResultList();
+        for(Customer customer : result){
+            System.out.println(customer);
+        }
+        // 4.提交事务
+        transaction.commit();
+        //5.释放资源
+        em.close();
+    }
+
+}
