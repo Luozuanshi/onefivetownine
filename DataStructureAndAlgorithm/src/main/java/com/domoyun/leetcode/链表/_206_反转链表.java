aa10e17d735f02fc8791910b7a1cc1b7c2905904
pangluo <59036386@qq.com>
2021-08-29 23:00:25 +0800

    非递归方式反转链表

--- DataStructureAndAlgorithm/src/main/java/com/domoyun/leetcode/链表/_206_反转链表.java
+++ DataStructureAndAlgorithm/src/main/java/com/domoyun/leetcode/链表/_206_反转链表.java
@@ -6,6 +6,11 @@ package com.domoyun.leetcode.链表;
  * 反转  1-2-3-4-5
  */
 public class _206_反转链表 {
+    /**
+     * 递归方式
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return null;
 
@@ -14,4 +19,23 @@ public class _206_反转链表 {
         head.next = null;
         return newHead;
     }
+
+    /**
+     * 非递归方式
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) return null;
+
+        ListNode newHead = null;
+        while (head!=null){
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
+    }
+
 }
